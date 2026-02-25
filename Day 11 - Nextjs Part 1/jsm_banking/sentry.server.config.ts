f784d90229a7e6f701d1c855f8d7b380080ad17c
DeveloperSAJ <developersaj981@gmail.com>
2026-02-25 23:31:53 +0500

    Banking App

---
+++ Day 11 - Nextjs Part 1/jsm_banking/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://48fe540a748ad937c131773db110a196@o4510946551201792.ingest.us.sentry.io/4510946589409280",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
